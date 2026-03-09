67eaba2995d089b291d7b09b2594cb3ac447c06d
saberzero1 <github@emilebangma.com>
2026-03-09 03:04:03 +0100

    deps: update @quartz-community/types and @quartz-community/utils
    Also fix code formatting with prettier.

--- src/components/scripts/readermode.inline.ts
+++ src/components/scripts/readermode.inline.ts
@@ -1,25 +1,25 @@
-let isReaderMode = false
+let isReaderMode = false;
 
 const emitReaderModeChangeEvent = (mode: "on" | "off") => {
   const event: CustomEventMap["readermodechange"] = new CustomEvent("readermodechange", {
     detail: { mode },
-  })
-  document.dispatchEvent(event)
-}
+  });
+  document.dispatchEvent(event);
+};
 
 document.addEventListener("nav", () => {
   const switchReaderMode = () => {
-    isReaderMode = !isReaderMode
-    const newMode = isReaderMode ? "on" : "off"
-    document.documentElement.setAttribute("reader-mode", newMode)
-    emitReaderModeChangeEvent(newMode)
-  }
+    isReaderMode = !isReaderMode;
+    const newMode = isReaderMode ? "on" : "off";
+    document.documentElement.setAttribute("reader-mode", newMode);
+    emitReaderModeChangeEvent(newMode);
+  };
 
   for (const readerModeButton of document.getElementsByClassName("readermode")) {
-    readerModeButton.addEventListener("click", switchReaderMode)
-    window.addCleanup(() => readerModeButton.removeEventListener("click", switchReaderMode))
+    readerModeButton.addEventListener("click", switchReaderMode);
+    window.addCleanup(() => readerModeButton.removeEventListener("click", switchReaderMode));
   }
 
   // Set initial state
-  document.documentElement.setAttribute("reader-mode", isReaderMode ? "on" : "off")
-})
+  document.documentElement.setAttribute("reader-mode", isReaderMode ? "on" : "off");
+});
